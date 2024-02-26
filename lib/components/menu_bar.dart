55b64eda12afc597ccd64dd72721bd54ff39e4d5
Meri11Ra1 <r.t.00293794@gmail.com>
2024-02-26 23:14:38 +0900

    郵便番号を保存できるように

--- lib/components/menu_bar.dart
+++ lib/components/menu_bar.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
 import '../provider/auth_service.dart';
 
-class MyMenuBar extends StatelessWidget {
+
+class MyMenuBar extends StatefulWidget {
   MyMenuBar({super.key});
 
+  @override
+  State<MyMenuBar> createState() => _MyMenuBarState();
+}
+
+class _MyMenuBarState extends State<MyMenuBar> {
   final AuthService _authService = AuthService();
+
+  String postcode = '';
+  String postcodeFirst = '';
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    postcode = '9052171';
+    postcodeFirst = postcode.substring(0, 3);
+  }
 
   @override
   Widget build(BuildContext context) {
