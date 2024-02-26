b21a34371e09c1be5d9ca752f5048f54169a4aa8
Meri11Ra1 <r.t.00293794@gmail.com>
2024-02-27 00:30:09 +0900

    郵便番号から選挙区を取得できるようにした

--- lib/components/menu_bar.dart
+++ lib/components/menu_bar.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart' show rootBundle;
+import 'dart:convert';
 import '../provider/auth_service.dart';
 
 
@@ -12,8 +14,49 @@ class MyMenuBar extends StatefulWidget {
 class _MyMenuBarState extends State<MyMenuBar> {
   final AuthService _authService = AuthService();
 
+  // 郵便番号
   String postcode = '';
   String postcodeFirst = '';
+  
+  //選挙区コード
+  String prefectureCode = '';
+  String districtNum = '';
+
+  // 選挙区
+  String prefectureText = '';
+  String hireiDistrictText = '';
+  String electoralDistrictText = '';
+
+  // 郵便番号から選挙区を取得
+  Future<void> loadJsonAsset() async {
+    final String hireiJsonString = await rootBundle.loadString('assets/json/hirei.json');
+    final dynamic hireiJsonResponse = json.decode(hireiJsonString);
+    final String postalJsonString = await rootBundle.loadString('assets/json/postal2senkyoku.light.json');
+    final dynamic postalJsonResponse = json.decode(postalJsonString);
+    setState(() {
+      try{
+        if (postalJsonResponse[postcodeFirst] != null) {
+          prefectureCode = postalJsonResponse[postcodeFirst]['p'];
+          districtNum = postalJsonResponse[postcodeFirst]['s'];
+        } else {
+          prefectureCode = postalJsonResponse[postcode]['p'];
+          districtNum = postalJsonResponse[postcode]['s'];
+        }
+        prefectureText = hireiJsonResponse[prefectureCode]['prefecture'];
+        hireiDistrictText = hireiJsonResponse[prefectureCode]['region'];
+
+        electoralDistrictText = '$prefectureText $districtNum区';
+      } catch (e) {
+        prefectureText = 'エラー';
+        hireiDistrictText = 'エラー';
+        electoralDistrictText = 'エラー';
+      }
+    });
+
+    debugPrint(prefectureText);
+    debugPrint(hireiDistrictText);
+    debugPrint(electoralDistrictText);
+  }
 
   @override
   void initState() {
@@ -21,6 +64,8 @@ class _MyMenuBarState extends State<MyMenuBar> {
     super.initState();
     postcode = '9052171';
     postcodeFirst = postcode.substring(0, 3);
+
+    loadJsonAsset();
   }
 
   @override
@@ -28,9 +73,43 @@ class _MyMenuBarState extends State<MyMenuBar> {
     return Drawer(
       child: Column(
         children: [
-          const DrawerHeader(
-            padding: EdgeInsets.fromLTRB(0, 25, 0, 0),
-            child: Text('Menu Bar'),
+          DrawerHeader(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  height: 0,
+                  width: 300,
+                ),
+                Text(
+                  '選挙区',
+                  style: TextStyle(
+                    fontSize: 20,
+                  )
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 4.0),
+                  child: Text(
+                    '比例区  $hireiDistrictTextブロック',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    )
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 4.0),
+                  child: Text(
+                    '小選挙区  $electoralDistrictText',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    )
+                  ),
+                ),
+                Text('郵便番号: $postcode'),
+              ],
+            ),
           ),
           ListTile(
             title: const Text('Sign Out'),
