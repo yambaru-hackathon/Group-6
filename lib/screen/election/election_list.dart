2da51d4595bbb7f08aa3451a0c0558deb7b01acc
Meri11Ra1 <r.t.00293794@gmail.com>
2024-02-29 22:57:33 +0900

    表示する選挙一覧をuserDataによって制御

--- lib/screen/election/election_list.dart
+++ lib/screen/election/election_list.dart
@@ -4,6 +4,10 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../components/app_bar.dart';
 import 'lower_house.dart';
 
+//local
+
+import '../../provider/auth_state.dart';
+
 class ElectionList extends ConsumerStatefulWidget {
   const ElectionList({super.key});
 
@@ -12,7 +16,6 @@ class ElectionList extends ConsumerStatefulWidget {
 }
 
 class ListGenerator {
-
   static Map<String, dynamic> generateList(int i) {
     Map<String, dynamic> map = {};
 
@@ -46,9 +49,12 @@ class TwoDimensionalListGenerator {
 }
 
 class _ElectionListState extends ConsumerState<ElectionList> {
-  final CollectionReference<Map<String, dynamic>> electionData = FirebaseFirestore.instance.collection('elections');
+  final CollectionReference<Map<String, dynamic>> electionData =
+      FirebaseFirestore.instance.collection('elections');
   var lowerHouseData;
   var upperHouseData;
+
+  final users = FirebaseFirestore.instance.collection('users');
 
   final _controller = FixedExtentScrollController(initialItem: 0);
   String page = 'test';
@@ -75,11 +81,37 @@ class _ElectionListState extends ConsumerState<ElectionList> {
     });
   }
 
+  void _fetchUserVote() async {
+    // データベースからユーザーの投票履歴を取得
+    lowerHouseData = await electionData.doc('lowerHouse').get();
+    upperHouseData = await electionData.doc('upperHouse').get();
+
+    final uid = ref.read(userIdProvider);
+    final userData = await users.doc(uid).get();
+    final String lowerHouseVote = userData['lowerHouseVote'];
+
+    // 選挙が終了している場合、選挙リストから削除
+    // 衆議院選挙
+    setState(() {
+      if (lowerHouseVote.isNotEmpty) {
+        // debugPrint('vote history of user is fetched');
+        _electionList.removeWhere((element) => element['id'] == lowerHouseData['name']);
+      }
+    });
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    _fetchData();
+  }
+
   int _selectedItemIndex = 0;
   @override
   Widget build(BuildContext context) {
-    _fetchData();
-
+    _fetchUserVote();
     return Scaffold(
       appBar: myAppBar(context, '選挙を選ぶ'),
       body: Container(
@@ -89,110 +121,177 @@ class _ElectionListState extends ConsumerState<ElectionList> {
         child: Column(
 //          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            SizedBox(
-              height: 400,
-              child: ListWheelScrollView(
-                controller: _controller,
-                diameterRatio: 50, //リストの間の幅
-                itemExtent: 150, //リストの幅
-//                overAndUnderCenterOpacity: 0.5, //透明度
-                perspective: 0.0001, //まるみ
-                useMagnifier: false, //拡大するか否か
-                magnification: 1, //拡大のどあい
-                physics: FixedExtentScrollPhysics(),
-                onSelectedItemChanged: (int index) {
-                  // update the UI on selected item changes
-                  setState(() {
-                    _selectedItemIndex = index;
-                    page = _electionList[index]['page'];
-                  });
-                },
-                children: [
-                  for (var map in _electionList)
-                    Container(
-                      width: 500,
-                      height: 300,
-                      child: Card(
-                        shadowColor: Colors.black,
-                        elevation: _selectedItemIndex == map['order'] ? 30 : 0,
-                        margin: _selectedItemIndex == map['order']
-                            ? const EdgeInsets.only(
-                                right: 60,
-                                left: 60,
-                              )
-                            : _selectedItemIndex < map['order']
-                                ? const EdgeInsets.only(
-                                    top: 10,
-                                    bottom: 30,
-                                    right: 90,
-                                    left: 90,
-                                  )
-                                : const EdgeInsets.only(
-                                    top: 30,
-                                    bottom: 10,
-                                    right: 90,
-                                    left: 90,
-                                  ),
-                        color: _selectedItemIndex == map['order']
-                            ? Color.fromARGB(255, 99, 112, 255)
-                            : _selectedItemIndex + 1 == map['order'] ||
-                                    _selectedItemIndex - 1 == map['order']
-                                ? Colors.white
-                                : Colors.transparent,
-                        shape: RoundedRectangleBorder(
-                          side: BorderSide(
-                            color: _selectedItemIndex + 1 == map['order'] ||
-                                    _selectedItemIndex - 1 == map['order']
-                                ? const Color.fromARGB(255, 50, 61, 180)
-                                : Colors.transparent, //色
-                            width: 2, //太さ
+            Stack(children: [
+              SizedBox(
+                height: 400,
+                child: ListWheelScrollView(
+                  controller: _controller,
+                  diameterRatio: 50, //リストの間の幅
+                  itemExtent: 150, //リストの幅
+                  //                overAndUnderCenterOpacity: 0.5, //透明度
+                  perspective: 0.0001, //まるみ
+                  useMagnifier: false, //拡大するか否か
+                  magnification: 1, //拡大のどあい
+                  physics: FixedExtentScrollPhysics(),
+                  onSelectedItemChanged: (int index) {
+                    // update the UI on selected item changes
+                    setState(() {
+                      _selectedItemIndex = index;
+                      page = _electionList[index]['page'];
+                    });
+                  },
+                  children: [
+                    for (var map in _electionList)
+                      Container(
+                        width: 500,
+                        height: 300,
+                        child: Card(
+                          shadowColor: Colors.black,
+                          elevation:
+                              _selectedItemIndex == map['order'] ? 30 : 0,
+                          margin: _selectedItemIndex == map['order']
+                              ? const EdgeInsets.only(
+                                  right: 60,
+                                  left: 60,
+                                )
+                              : _selectedItemIndex < map['order']
+                                  ? const EdgeInsets.only(
+                                      top: 10,
+                                      bottom: 30,
+                                      right: 90,
+                                      left: 90,
+                                    )
+                                  : const EdgeInsets.only(
+                                      top: 30,
+                                      bottom: 10,
+                                      right: 90,
+                                      left: 90,
+                                    ),
+                          color: _selectedItemIndex == map['order']
+                              ? Color.fromARGB(255, 99, 112, 255)
+                              : _selectedItemIndex + 1 == map['order'] ||
+                                      _selectedItemIndex - 1 == map['order']
+                                  ? Colors.white
+                                  : Colors.transparent,
+                          shape: RoundedRectangleBorder(
+                            side: BorderSide(
+                              color: _selectedItemIndex + 1 == map['order'] ||
+                                      _selectedItemIndex - 1 == map['order']
+                                  ? const Color.fromARGB(255, 50, 61, 180)
+                                  : Colors.transparent, //色
+                              width: 2, //太さ
+                            ),
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(10),
+                              // BorderRadius.only(
+                              //                            topLeft: _selectedItemIndex < map['order']
+                              //                                ? Radius.zero
+                              //                                : Radius.circular(10),
+                              //                            topRight: _selectedItemIndex < map['order']
+                              //                                ? Radius.zero
+                              //                                : Radius.circular(10),
+                              //                            bottomLeft: _selectedItemIndex > map['order']
+                              //                                ? Radius.zero
+                              //                                : Radius.circular(10),
+                              //                            bottomRight: _selectedItemIndex > map['order']
+                              //                                ? Radius.zero
+                              //                                : Radius.circular(10),
+                            ),
                           ),
-                          borderRadius: BorderRadius.all(
-                            Radius.circular(10),
-                            // BorderRadius.only(
-//                            topLeft: _selectedItemIndex < map['order']
-//                                ? Radius.zero
-//                                : Radius.circular(10),
-//                            topRight: _selectedItemIndex < map['order']
-//                                ? Radius.zero
-//                                : Radius.circular(10),
-//                            bottomLeft: _selectedItemIndex > map['order']
-//                                ? Radius.zero
-//                                : Radius.circular(10),
-//                            bottomRight: _selectedItemIndex > map['order']
-//                                ? Radius.zero
-//                                : Radius.circular(10),
-                          ),
-                        ),
-                        child: InkWell(
-                          splashColor: const Color.fromARGB(255, 148, 207, 255)
-                              .withAlpha(30),
-                          onTap: () {
-                            setState(() {
-                              _selectedItemIndex = map['order'];
-                              _scroll(map['order']);
-                              page = map['page'];
-                            });
-                            debugPrint('Card taped');
-                          },
-                          child: Center(
-                            child: Text(
-                              _selectedItemIndex == map['order']
-                                  ? (map['id']) +
-                                      '\n' +
-                                      (map['date']).toString()
-                                  : (map['id']),
-                              style: TextStyle(
-                                fontSize: 20,
+                          child: InkWell(
+                            splashColor:
+                                const Color.fromARGB(255, 148, 207, 255)
+                                    .withAlpha(30),
+                            onTap: () {
+                              setState(() {
+                                _selectedItemIndex = map['order'];
+                                _scroll(map['order']);
+                                page = map['page'];
+                              });
+                              debugPrint('Card taped');
+                            },
+                            child: Center(
+                              child: Text(
+                                _selectedItemIndex == map['order']
+                                    ? (map['id']) +
+                                        '\n' +
+                                        (map['date']).toString()
+                                    : (map['id']),
+                                style: TextStyle(
+                                  fontSize: 20,
+                                ),
                               ),
                             ),
                           ),
                         ),
                       ),
-                    ),
-                ],
+                  ],
+                ),
               ),
-            ),
+
+              // 選挙リストがない場合、選挙リストがないことを表示
+              _electionList.length == 0
+                  ? SizedBox(
+                      height: 400,
+                      child: ListWheelScrollView(
+                        controller: _controller,
+                        diameterRatio: 50, //リストの間の幅
+                        itemExtent: 150, //リストの幅
+                        //                overAndUnderCenterOpacity:0.5, //透明度
+                        perspective: 0.0001, //まるみ
+                        useMagnifier: false, //拡大するか否か
+                        magnification: 1, //拡大のどあい
+                        physics: FixedExtentScrollPhysics(),
+                        onSelectedItemChanged: (int index) {
+                          // update the UI on selected item changes
+                          setState(() {
+                            _selectedItemIndex = index;
+                          });
+                        },
+                        children: [
+                          Container(
+                            width: 500,
+                            height: 300,
+                            child: Card(
+                              shadowColor: Colors.black,
+                              elevation: 30,
+                              margin: const EdgeInsets.only(
+                                right: 60,
+                                left: 60,
+                              ),
+                              color: Color.fromARGB(255, 99, 112, 255),
+                              shape: RoundedRectangleBorder(
+                                side: BorderSide(
+                                  color: const Color.fromARGB(255, 50, 61, 180),
+                                  width: 2, //太さ
+                                ),
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(10),
+                                ),
+                              ),
+                              child: InkWell(
+                                splashColor:
+                                    const Color.fromARGB(255, 148, 207, 255)
+                                        .withAlpha(30),
+                                onTap: () {
+                                  debugPrint('Card taped');
+                                },
+                                child: Center(
+                                  child: Text(
+                                    'Coming soon',
+                                    style: TextStyle(
+                                      fontSize: 20,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                  : SizedBox(), // 選挙リストがある場合何も表示しない
+            ]),
             ElevatedButton(
               onPressed: () {
                 if (page == 'lower_house') {
@@ -200,17 +299,9 @@ class _ElectionListState extends ConsumerState<ElectionList> {
                     context,
                     MaterialPageRoute(builder: (context) => LowerHouse()),
                   );
-                } else if (page == 'upper_house') {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => LowerHouse()),
-                  );
-                } else if (page == 'AnotherHouse') {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => LowerHouse()),
-                  );
-                }
+                } // else if (page == 'upper_house') {
+                //
+                //}
               },
               child: const Text('投票画面へGO!'),
             ),
