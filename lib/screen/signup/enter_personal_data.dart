98e0bcd17608018e30c81063adb996b55dfa8e9b
Meri11Ra1 <r.t.00293794@gmail.com>
2024-03-01 16:51:20 +0900

    formの情報の保存と画面遷移してもインプット情報が残る

--- lib/screen/signup/enter_personal_data.dart
+++ lib/screen/signup/enter_personal_data.dart
@@ -3,11 +3,19 @@ import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 import 'package:path/path.dart';
 import '../App.dart';
-
 import 'dart:io';
-
 import 'package:camera/camera.dart';
 
+//firebase
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+final users = FirebaseFirestore.instance.collection('users');
+
+final ProfileData _data = ProfileData();
+final myNumberController = TextEditingController();
+final postcodeController = TextEditingController();
+bool isSaveText = false;
 
 Future<void> bulidCamera() async {
   // main 関数内で非同期処理を呼び出すための設定
@@ -18,7 +26,6 @@ Future<void> bulidCamera() async {
   final firstCamera = cameras.first;
   runApp(MyApp(camera: firstCamera));
 }
-
 
 class MyApp extends StatelessWidget {
   const MyApp({
@@ -120,6 +127,7 @@ class DisplayPictureScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(title: const Text('')),
       body: Center(
         child: Column(
           children: [
@@ -146,56 +154,130 @@ class DisplayPictureScreen extends StatelessWidget {
   }
 }
 
-class EnterPersonalData extends StatefulWidget {
+// 最初の画面
+class EnterPersonalData extends StatefulWidget { 
 
   @override
-  State<EnterPersonalData> createState() => _EnterPersonalDataState();
+  State<EnterPersonalData> createState() => EnterPersonalDataState();
 }
 
-class _EnterPersonalDataState extends State<EnterPersonalData> {
+class ProfileData {
+  String postcode = '';
+  String myNumber = '';
+}
+
+FormFieldValidator _requiredValidator(BuildContext context) =>
+    (val) => val.isEmpty ? "必須" : null;
+
+class EnterPersonalDataState extends State<EnterPersonalData> {
+  // FormField
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  final uid = FirebaseAuth.instance.currentUser!.uid;
+
+  Future<void> submit() async {
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.save();
+
+      debugPrint('郵便番号: ${_data.postcode}');
+      debugPrint('マイナンバー: ${_data.myNumber}');
+
+      users.doc(uid).update({
+        'postcode': _data.postcode,
+        'myNumber': _data.myNumber,
+      });
+    }
+  }
+
+  @override
+  void dispose() {
+    debugPrint(postcodeController.text);
+    debugPrint(myNumberController.text);
+    if (!isSaveText){
+      myNumberController.dispose();
+      postcodeController.dispose();
+    }
+    // TODO: implement dispose
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
+    debugPrint(_data.postcode);
+    debugPrint(_data.myNumber);
     return Scaffold(
-      appBar: AppBar(
-      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Center(
               child: Text(
-                '投票を行うには、本人認証が必要です。\n今はスキップして、あとで行うこともできます。',
+                '投票を行うには、本人認証が必要です。\n今はスキップして、あとで行うこともできます。\nこれらの情報はあとから変更することができません。',
                 style: TextStyle(fontSize: 12),
                 textAlign: TextAlign.center,
-                
               ),
             ),
+            SizedBox(height: 24),
+            // 情報入力フィールド
             Container(
-              child: Column(
-                children:[
-                  Container(
-                    width: 240,
-                    height: 48,
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Color.fromARGB(255, 137, 198, 179),
-                        width: 3,
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    SizedBox(
+                      width: 240,
+                      height: 90,
+                      child: TextFormField(
+                        controller: postcodeController,
+                        decoration: InputDecoration(
+                          labelText: '郵便番号',
+                          hintText: '0000000',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                        validator: _requiredValidator(context),
+                        keyboardType: TextInputType.number,
+                        inputFormatters: <TextInputFormatter>[
+                          FilteringTextInputFormatter.digitsOnly,
+                        ],
                       ),
-                      borderRadius: BorderRadius.circular(10),
                     ),
-                    child: NumberInputField(),
-                  ),
-                ],
+                    // マイナンバー
+                    SizedBox(
+                      width: 240,
+                      height: 90,
+                      child: TextFormField(
+                        controller: myNumberController,
+                        decoration: InputDecoration(
+                          labelText: 'マイナンバー',
+                          hintText: '000000000000',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                        validator: _requiredValidator(context),
+                        onSaved: (String? value) => _data.myNumber = value!,
+                        keyboardType: TextInputType.number,
+                        inputFormatters: <TextInputFormatter>[
+                          FilteringTextInputFormatter.digitsOnly,
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
+            // カメラ
             Container(
-              padding: EdgeInsets.only(top: 28),
+              padding: EdgeInsets.only(bottom: 24),
               child: Column(
                 children: [
-                  GestureDetector(  //this
-                    onTap: () {
-                      print('tapped');                      
+                  GestureDetector(
+                    //this
+                    onTap: () async {
+                      print('tapped');
+                      isSaveText = await true;
                       /*Navigator.push(
                         context,
                         MaterialPageRoute(builder: (context) => TakePictureScreen()), // カメラ情報を渡す
@@ -207,18 +289,73 @@ class _EnterPersonalDataState extends State<EnterPersonalData> {
                       height: 150,
                       decoration: BoxDecoration(
                         border: Border.all(
-                          color: Color.fromARGB(255, 137, 198, 179),
-                          width: 3,
+                          color: Color.fromARGB(255, 146, 146, 146),
+                          width: 1,
                         ),
                         borderRadius: BorderRadius.circular(10),
                       ),
                       child: Center(
-                        child: 
-                        Icon(Icons.camera_alt_rounded, size: 36, color: Color.fromARGB(255, 146, 146, 146),),
+                        child: Icon(
+                          Icons.camera_alt_rounded,
+                          size: 36,
+                          color: Color.fromARGB(255, 146, 146, 146),
+                        ),
                       ),
                     ),
                   ),
                 ],
+              ),
+            ),
+            // 登録ボタン
+            Container(
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Color.fromARGB(255, 137, 198, 179),
+                  width: 3,
+                ),
+                borderRadius: BorderRadius.circular(45),
+              ),
+              child: SizedBox(
+                width: 150,
+                height: 50,
+                child: TextButton(
+                  onPressed: () {
+                    submit();
+                  },
+                  child: Text(
+                    '登録完了',
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.black87,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 10),
+            Container(
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Color.fromARGB(255, 137, 198, 179),
+                  width: 3,
+                ),
+                borderRadius: BorderRadius.circular(3),
+              ),
+              child: SizedBox(
+                width: 100,
+                height: 40,
+                child: TextButton(
+                  onPressed: () {
+                    submit();
+                  },
+                  child: Text(
+                    'あとで',
+                    style: TextStyle(
+                      fontSize: 15,
+                      color: Colors.black87,
+                    ),
+                  ),
+                ),
               ),
             ),
           ],
@@ -227,40 +364,3 @@ class _EnterPersonalDataState extends State<EnterPersonalData> {
     );
   }
 }
-
-class NumberInputField extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: 200,
-      child: TextField(
-        decoration: InputDecoration(
-          hintText: 'マイナンバーを入力してください',
-          
-        ),
-        keyboardType: TextInputType.number,
-        inputFormatters: <TextInputFormatter>[
-          FilteringTextInputFormatter.digitsOnly,
-        ],
-      ),
-    );
-  }
-}
-
-/*
-body: Center(
-  child: Column(
-    mainAxisAlignment: MainAxisAlignment.center,
-    children: <Widget>[
-      const Text(
-        'This is enter personal data screen.',
-      ),
-      ElevatedButton(
-        onPressed: () {Navigator.push(
-          context,
-          MaterialPageRoute(builder: (context) => const AppScreen())
-        );},
-        child: const Text('to Home Screen')),
-    ],
-  ),
-),*/
