0ea9f08f8a3a7cdbff922a056f9c66b7da485535
Meri11Ra1 <r.t.00293794@gmail.com>
2024-03-01 10:39:49 +0900

    signup to enterPersonalData

--- lib/screen/signup/sign_up.dart
+++ lib/screen/signup/sign_up.dart
@@ -12,14 +12,17 @@ class SignUp extends StatefulWidget {
   @override
   State<SignUp> createState() => _SignUpState();
 }
-class _ProfileData{
+
+class _ProfileData {
   String email = '';
   String emailAgain = '';
   String pass = '';
   String passAgain = '';
 }
+
 // 必須チェック
-FormFieldValidator _requiredValidator(BuildContext context) => (val) => val.isEmpty ? "必須" : null;
+FormFieldValidator _requiredValidator(BuildContext context) =>
+    (val) => val.isEmpty ? "必須" : null;
 
 class _SignUpState extends State<SignUp> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
@@ -39,14 +42,14 @@ class _SignUpState extends State<SignUp> {
   // 入力の重複用ブール
   bool passMismatch = false;
   bool emailMismatch = false;
-  
+
   // signIn失敗のブール
   bool signInFailed = false;
-  
+
   // 入力の表示非表示
   bool _isObscure = true;
   bool _isObscureAgain = true;
-  
+
   bool email_Wrongformat = false;
   bool pass_Wrongformat = false;
 
@@ -60,37 +63,42 @@ class _SignUpState extends State<SignUp> {
       _nameFocusNode.unfocus();
       _descriptionFocusNode.unfocus();
       try {
-      UserCredential userCredential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
-        email: _data.email,
-        password: _data.pass,
-      );
-      await FirebaseFirestore.instance.collection('users').doc(userCredential.user!.uid).set({
-        'lowerHouseVote': '',
-        'myNumber': '',
-        'postcode': '',
-        'prefecture': '',
-        'senkyokuNum': ''
-      });
+        UserCredential userCredential =
+            await FirebaseAuth.instance.createUserWithEmailAndPassword(
+          email: _data.email,
+          password: _data.pass,
+        );
+        await FirebaseFirestore.instance
+            .collection('users')
+            .doc(userCredential.user!.uid)
+            .set({
+          'lowerHouseVote': '',
+          'myNumber': '',
+          'postcode': '',
+          'prefecture': '',
+          'senkyokuNum': ''
+        });
+        Navigator.push(context,
+            MaterialPageRoute(builder: (context) => EnterPersonalData()));
       } on FirebaseAuthException catch (e) {
+        if (e.code == 'email-already-in-use') {
+          print('指定したメールアドレスは登録済みです');
+        } else if (e.code == 'invalid-email') {
+          print('メールアドレスのフォーマットが正しくありません');
 
-       if (e.code == 'email-already-in-use') {
-         print('指定したメールアドレスは登録済みです');
-         
-       } else if (e.code == 'invalid-email') {
-         print('メールアドレスのフォーマットが正しくありません');
+          setState(() {
+            email_Wrongformat = true;
+          });
+        } else if (e.code == 'operation-not-allowed') {
+          print('指定したメールアドレス・パスワードは現在使用できません');
+        } else if (e.code == 'weak-password') {
+          print('パスワードは６文字以上にしてください');
 
-        setState(() {
-          email_Wrongformat = true;
-        });
-       } else if (e.code == 'operation-not-allowed') {
-         print('指定したメールアドレス・パスワードは現在使用できません');
-       } else if (e.code == 'weak-password') {
-         print('パスワードは６文字以上にしてください');
+          setState(() {
+            pass_Wrongformat = true;
+          });
+        }
 
-        setState(() {
-          pass_Wrongformat = true;
-        });
-       }
         /// その他エラー
         else {
           print('アカウント作成エラー: ${e.code}');
@@ -98,12 +106,9 @@ class _SignUpState extends State<SignUp> {
       } catch (e) {
         print(e);
       }
-      Navigator.push(
-          context,
-          MaterialPageRoute(builder: (context) => EnterPersonalData())
-        );
     }
   }
+
   @override
   void initState() {
     super.initState();
@@ -127,8 +132,7 @@ class _SignUpState extends State<SignUp> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-      ),
+      appBar: AppBar(),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -145,7 +149,9 @@ class _SignUpState extends State<SignUp> {
                     borderRadius: BorderRadius.circular(10),
                   ),
                 ),
-                const SizedBox(height: 0,),
+                const SizedBox(
+                  height: 0,
+                ),
                 // Container(
                 //   height: 5,
                 //   width: 200,
@@ -154,16 +160,17 @@ class _SignUpState extends State<SignUp> {
                 //     borderRadius: BorderRadius.circular(10),
                 //   ),
                 // ),
-                const SizedBox(height: 20,),
+                const SizedBox(
+                  height: 20,
+                ),
               ],
             ),
             const Text(
               'SignUp',
               style: TextStyle(
-                fontSize: 24,
-                // fontWeight: FontWeight.bold,
-                fontFamily: 'Murecho'
-              ),
+                  fontSize: 24,
+                  // fontWeight: FontWeight.bold,
+                  fontFamily: 'Murecho'),
             ),
             Text(
               signInFailed ? 'password or email is wrong' : '',
@@ -171,140 +178,148 @@ class _SignUpState extends State<SignUp> {
                 color: Colors.red,
               ),
             ),
-            const SizedBox(height: 5,),
+            const SizedBox(
+              height: 5,
+            ),
             Expanded(
               child: Padding(
-                padding: const EdgeInsets.all(32.0),
-                child: Form(
-                  key: _formKey,
-                  child: ListView(
-                    children: [
-                      //eメール入力フォーム(ノーマル)
-                      TextFormField(
-                        decoration: const InputDecoration(labelText: 'email', border: OutlineInputBorder()),
-                        validator: _requiredValidator(context),
-                        maxLength: 100,
-                        maxLengthEnforcement: MaxLengthEnforcement.enforced,
-                        focusNode: _nameFocusNode,
-                        onSaved: (String? value) => _data.email = value!,
-              
-                        // focus当てとく
-                        autofocus: true,
-              
-                        // focus移動
-                        textInputAction: TextInputAction.next,
-                        onFieldSubmitted: (_) => FocusScope.of(context).requestFocus(_descriptionFocusNode),
+                  padding: const EdgeInsets.all(32.0),
+                  child: Form(
+                    key: _formKey,
+                    child: ListView(
+                      children: [
+                        //eメール入力フォーム(ノーマル)
+                        TextFormField(
+                          decoration: const InputDecoration(
+                              labelText: 'email', border: OutlineInputBorder()),
+                          validator: _requiredValidator(context),
+                          maxLength: 100,
+                          maxLengthEnforcement: MaxLengthEnforcement.enforced,
+                          focusNode: _nameFocusNode,
+                          onSaved: (String? value) => _data.email = value!,
 
-                        // 重複用
-                        controller: emailin,
-                      ),
-                      const SizedBox(height: 5.0),
-                      //eメール入力フォーム(アゲイン)
-                      TextFormField(
-                        decoration: InputDecoration(
-                          labelText: 'email Again', 
-                          border: OutlineInputBorder(),
-                          errorText: emailMismatch ? 'email do not match' : null,
+                          // focus当てとく
+                          autofocus: true,
+
+                          // focus移動
+                          textInputAction: TextInputAction.next,
+                          onFieldSubmitted: (_) => FocusScope.of(context)
+                              .requestFocus(_descriptionFocusNode),
+
+                          // 重複用
+                          controller: emailin,
+                        ),
+                        const SizedBox(height: 5.0),
+                        //eメール入力フォーム(アゲイン)
+                        TextFormField(
+                          decoration: InputDecoration(
+                            labelText: 'email Again',
+                            border: OutlineInputBorder(),
+                            errorText:
+                                emailMismatch ? 'email do not match' : null,
                           ),
-                        validator: _requiredValidator(context),
-                        maxLength: 100,
-                        maxLengthEnforcement: MaxLengthEnforcement.enforced,
-                        focusNode: _nameFocusNodeAgain,
-                        onSaved: (String? value) => _data.emailAgain = value!,
-                        // focus当てとく
-                        autofocus: true,
-              
-                        // focus移動
-                        textInputAction: TextInputAction.next,
-                        onFieldSubmitted: (_) => FocusScope.of(context).requestFocus(_descriptionFocusNode),
+                          validator: _requiredValidator(context),
+                          maxLength: 100,
+                          maxLengthEnforcement: MaxLengthEnforcement.enforced,
+                          focusNode: _nameFocusNodeAgain,
+                          onSaved: (String? value) => _data.emailAgain = value!,
+                          // focus当てとく
+                          autofocus: true,
 
-                        // 重複用
-                        controller: emailAgainin,
+                          // focus移動
+                          textInputAction: TextInputAction.next,
+                          onFieldSubmitted: (_) => FocusScope.of(context)
+                              .requestFocus(_descriptionFocusNode),
 
-                        onChanged: (value) {
-                          setState(() {
-                            emailMismatch = emailin.text != value;
-                          });
-                        },
-                      ),
-                      Visibility(
-                        child: Text('Please @ add'),
-                        visible: email_Wrongformat, // ここで隠すか表示するかを選択する
-                      ),
+                          // 重複用
+                          controller: emailAgainin,
 
-                      const SizedBox(height: 16.0),
-                      //パスワード入力フィールド(ノーマル)
-                      TextFormField(
-                        obscureText: _isObscure,
-                        decoration: InputDecoration(
-                          labelText: 'password', 
-                          border: const OutlineInputBorder(),
-                          suffixIcon: IconButton(
-                            onPressed: () {
-                              setState(() {
-                                _isObscure = !_isObscure;
-                              });
-                            }, 
-                            icon: Icon(_isObscure ? Icons.visibility_off : Icons.visibility)
-                            )
+                          onChanged: (value) {
+                            setState(() {
+                              emailMismatch = emailin.text != value;
+                            });
+                          },
                         ),
-                        validator: _requiredValidator(context),
-                        maxLength: 10,
-                        maxLengthEnforcement: MaxLengthEnforcement.enforced,
-                        focusNode: _descriptionFocusNode,
-                        onSaved: (String? value) => _data.pass = value!,
-                        // 複数行対応
-                        // keyboardType: TextInputType.multiline,
-                        // maxLines: null,
-
-                        // 重複用
-                        controller: passin,
-                      ),
-                      const SizedBox(height: 5.0),
-                      //パスワード入力フィールド(アゲイン)
-                      TextFormField(
-                        obscureText: _isObscureAgain,
-                        decoration: InputDecoration(
-                          labelText: 'password Again', 
-                          border: const OutlineInputBorder(),
-                          errorText: passMismatch ? 'password do not match' : null,
-                          suffixIcon: IconButton(
-                            onPressed: () {
-                              setState(() {
-                                _isObscureAgain = !_isObscureAgain;
-                              });
-                            }, 
-                            icon: Icon(_isObscureAgain ? Icons.visibility_off : Icons.visibility)
-                            ),
+                        Visibility(
+                          child: Text('Please @ add'),
+                          visible: email_Wrongformat, // ここで隠すか表示するかを選択する
                         ),
-                        validator: _requiredValidator(context),
-                        maxLength: 10,
-                        maxLengthEnforcement: MaxLengthEnforcement.enforced,
-                        focusNode: _descriptionFocusNodeAgain,
-                        onSaved: (String? value) => _data.passAgain = value!,
-                        // 複数行対応
-                        // keyboardType: TextInputType.multiline,
-                        // maxLines: null,
 
-                        // 重複用
-                        controller: passAgainin,
+                        const SizedBox(height: 16.0),
+                        //パスワード入力フィールド(ノーマル)
+                        TextFormField(
+                          obscureText: _isObscure,
+                          decoration: InputDecoration(
+                              labelText: 'password',
+                              border: const OutlineInputBorder(),
+                              suffixIcon: IconButton(
+                                  onPressed: () {
+                                    setState(() {
+                                      _isObscure = !_isObscure;
+                                    });
+                                  },
+                                  icon: Icon(_isObscure
+                                      ? Icons.visibility_off
+                                      : Icons.visibility))),
+                          validator: _requiredValidator(context),
+                          maxLength: 10,
+                          maxLengthEnforcement: MaxLengthEnforcement.enforced,
+                          focusNode: _descriptionFocusNode,
+                          onSaved: (String? value) => _data.pass = value!,
+                          // 複数行対応
+                          // keyboardType: TextInputType.multiline,
+                          // maxLines: null,
 
-                        onChanged: (value) {
-                          setState(() {
-                            passMismatch = passin.text != value;
-                          });
-                        },
-                      ),
-                      Visibility(
-                        child: Text('Please 6 or more characters'),
-                        visible: pass_Wrongformat, // ここで隠すか表示するかを選択する
-                      ),
-                      const SizedBox(height: 5.0),
-                      ElevatedButton(onPressed: _submit, child: const Text('Submit')),
-                    ],
-                  ),
-                )
-              ),
+                          // 重複用
+                          controller: passin,
+                        ),
+                        const SizedBox(height: 5.0),
+                        //パスワード入力フィールド(アゲイン)
+                        TextFormField(
+                          obscureText: _isObscureAgain,
+                          decoration: InputDecoration(
+                            labelText: 'password Again',
+                            border: const OutlineInputBorder(),
+                            errorText:
+                                passMismatch ? 'password do not match' : null,
+                            suffixIcon: IconButton(
+                                onPressed: () {
+                                  setState(() {
+                                    _isObscureAgain = !_isObscureAgain;
+                                  });
+                                },
+                                icon: Icon(_isObscureAgain
+                                    ? Icons.visibility_off
+                                    : Icons.visibility)),
+                          ),
+                          validator: _requiredValidator(context),
+                          maxLength: 10,
+                          maxLengthEnforcement: MaxLengthEnforcement.enforced,
+                          focusNode: _descriptionFocusNodeAgain,
+                          onSaved: (String? value) => _data.passAgain = value!,
+                          // 複数行対応
+                          // keyboardType: TextInputType.multiline,
+                          // maxLines: null,
+
+                          // 重複用
+                          controller: passAgainin,
+
+                          onChanged: (value) {
+                            setState(() {
+                              passMismatch = passin.text != value;
+                            });
+                          },
+                        ),
+                        Visibility(
+                          child: Text('Please 6 or more characters'),
+                          visible: pass_Wrongformat, // ここで隠すか表示するかを選択する
+                        ),
+                        const SizedBox(height: 5.0),
+                        ElevatedButton(
+                            onPressed: _submit, child: const Text('Submit')),
+                      ],
+                    ),
+                  )),
             ),
           ],
         ),
